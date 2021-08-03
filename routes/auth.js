0062f30d061b56d150891dc2d80e07bb0028f986
magus0 <harvestashya@gmail.com>
2021-08-02 17:08:19 -0700

    auth routes and uncommented tests

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,35 @@
 "use strict";
-const {
-	authenticateJWT,
-	ensureLoggedIn,
-	ensureCorrectUser,} = require('../middleware/auth');
 const User = require('../models/user');
-const {UnauthorizedError} = require('../expressError');
+const { BadRequestError } = require('../expressError');
+const { SECRET_KEY } = require('../config');
+const jwt = require("jsonwebtoken");
+
 const Router = require("express").Router;
 const router = new Router();
 
 /** POST /login: {username, password} => {token} */
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res, next) => {
 	const {username, password} = req.body;
-	if( User.authenticate(username, password) === true){
-		let token = jwt.sign({username}, SECRET_KEY);
-		return res.json({token});
-	} else {
-		throw new UnauthorizedError();
+	let user = await User.get(username);
+	if(user){
+		if( await User.authenticate(username, password) === true){
+			let token = jwt.sign({username}, SECRET_KEY);
+			return res.json({token});
+		}
 	}
+	throw new BadRequestError();
 });
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post('/register', async (req, res, next) => {
+	let user = await User.register(req.body);
+	let token = jwt.sign({username: user.username}, SECRET_KEY);
+	User.updateLoginTimestamp(user.username);
+	
+	return res.json({token});
+});
 
 module.exports = router;
